8654a57b7bc07952ee421aed74f488d017d34fa7
xing <582158539@qq.com>
2020-12-15 16:50:45 +0800

    Money.vue页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
     <Nav/>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-name: "Money"
-}
+  name: 'Money'
+};
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
