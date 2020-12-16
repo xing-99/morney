dafeb97fabb9fa47c7d691ec5472e8ed93b2d86a
xing <582158539@qq.com>
2020-12-16 14:25:44 +0800

    svg-sprite-loader-mod解决Webstorm提示问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,5 +13,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import "~@/assets/style/helper.scss";
 </style>
--- vue.config.js
+++ vue.config.js
@@ -19,11 +19,11 @@ module.exports = {
       .rule('svg-sprite')
       .test(/\.svg/)
       .include.add(dir).end() //包含icons目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
+      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
       .use('svgo-loader').loader('svgo-loader')
       .tap(options => ({...options, plugins: [{removeAttrs: {attr: 'fill'}}]})).end()
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) //其他svg loader排除icons目录
   }
 
