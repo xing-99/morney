b271e0551b74d942ea422b96a6440d4f4f7558a1
xing <582158539@qq.com>
2020-12-16 15:38:20 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,10 +13,12 @@
       统计
     </router-link>
   </nav>
+
 </template>
 
 <script lang="ts">
 import Icon from '@/components/Icon.vue';
+
 export default {
   name: 'Nav',
   components: {Icon}
@@ -24,25 +26,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  nav {
+@import "~@/assets/style/helper.scss";
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  flex-direction: row;
+  font-size: 12px;
+
+  > .item {
+    padding: 2px 0;
+    width: 33.333333%;
     display: flex;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-    flex-direction: row;
-    font-size: 12px;
-    > .item {
-      padding: 2px 0;
-      width: 33.333333%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      .icon {
-        width: 32px;
-        height: 32px;
-      }
-    }
-    > .item.selected {
-      color: pink;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    .icon {
+      width: 32px;
+      height: 32px;
     }
   }
+
+  > .item.selected {
+    color: $color-highlight;
+  }
+}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,53 @@
 <template>
-  <Layout>
-    Money.vue
-  </Layout>
+  <div>
+    <header>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </header>
+    <Layout>
+      <div>
+        <label class="output">
+          <span class="pay">支出</span>
+          <input type="text">
+        </label>
+      </div>
+      <div>
+        <label class="notes">
+          <span class="name">备注</span>
+          <input type="text">
+        </label>
+      </div>
+      <div class="tags">
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+      </div>
+      <div class="numberPad">
+        <div class="buttons">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </div>
+    </Layout>
+  </div>
+
 </template>
 
 <script lang="ts">
@@ -13,5 +59,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "~@/assets/style/helper.scss";
 </style>
