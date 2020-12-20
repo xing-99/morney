4285e136892cb4eeafe770f563789e95c387c7ad
xing <582158539@qq.com>
2020-12-20 19:44:28 +0800

    将tag的string改为{id, name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,8 +1,12 @@
 const localStorageKeyName = 'tagList';
 
+type Tag = {
+  id: string;
+  name: string;
+}
 type TagListModel = {
-  data: string[];
-  fetch: () => string[];
+  data: Tag[];
+  fetch: () => Tag[];
   create: (name: string) => 'success' | 'duplicated';
   save: () => void;
 }
@@ -11,17 +15,18 @@ const tagListModel: TagListModel = {
   data: [],
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
-    return this.data
+    return this.data;
   },
   save() {
-    return window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data))
+    return window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   },
   create(name: string) {
-    if (this.data.indexOf(name)>=0) {return 'duplicated'}
-    this.data.push(name);
-    this.save()
-    return 'success'
+    const names = this.data.map(item => item.name);
+    if (names.indexOf(name) >= 0) {return 'duplicated';}
+    this.data.push({id: name, name: name});
+    this.save();
+    return 'success';
   }
-}
+};
 
-export default tagListModel
+export default tagListModel;
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+
+  </Layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export default class EditLabel extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
