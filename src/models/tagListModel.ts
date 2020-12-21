b12b64d236a46640b89f6942f842b2a5d21024b4
xing <582158539@qq.com>
2020-12-21 15:05:07 +0800

    可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -8,7 +8,9 @@ type TagListModel = {
   data: Tag[];
   fetch: () => Tag[];
   create: (name: string) => 'success' | 'duplicated';
+  update: (id: string, name: string) => 'success' | 'duplicated' | 'not found';
   save: () => void;
+  remove: (id: string) => boolean;
 }
 
 const tagListModel: TagListModel = {
@@ -22,10 +24,43 @@ const tagListModel: TagListModel = {
   },
   create(name: string) {
     const names = this.data.map(item => item.name);
-    if (names.indexOf(name) >= 0) {return 'duplicated';}
-    this.data.push({id: name, name: name});
-    this.save();
-    return 'success';
+    if (names.indexOf(name) >= 0) {
+      return 'duplicated';
+    } else {
+      this.data.push({id: name, name: name});
+      this.save();
+      return 'success';
+    }
+  },
+  update(id, name) {
+    const idList = this.data.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.data.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return 'success';
+      }
+    } else {
+      return 'not found';
+    }
+  },
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    console.log('index');
+    console.log(index);
+    this.data.splice(index, 1);
+    this.save()
+    return true;
   }
 };
 
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,18 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click.native="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name"
+                @update:value="update"
+                field-name="标签名"
+                placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -24,16 +27,30 @@ import Button from '@/components/Button.vue';
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
+  tag?: { id: string; name: string} = undefined
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch()
     const tags = tagListModel.data
     const tag = tags.filter(t => t.id === id)[0];//一定会返回数组
     if(tag) {
-      console.log(tag);
+      this.tag = tag
     } else {
       this.$router.replace('/404')
     }
+  }
+  update(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name)
+    }
+  }
+  remove() {
+    if (this.tag) {
+      tagListModel.remove(this.tag.id)
+    }
+  }
+  goBack() {
+    this.$router.back()
   }
 }
 </script>
