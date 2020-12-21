15de8e65a9dbb2296baf1693b81ab8644cebc0e1
xing <582158539@qq.com>
2020-12-21 16:09:49 +0800

    将create封装到recordListModel.ts

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,16 +1,20 @@
+import clone from '@/lib/clone';
+
 const localStorageKeyName = 'recordList'
 
 const recordListModel = {
   data: [] as RecordItem[],
-  clone(data: RecordItem[] | RecordItem) {
-    return JSON.parse(JSON.stringify(data))
-  },
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     return this.data
   },
   save() {
     return window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data))
+  },
+  create(record: RecordItem[]) {
+    const record2 = clone(record);
+    record2.createdAt = new Date();
+    this.data.push(record2);
   }
 }
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -49,9 +49,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2 = recordListModel.clone(this.record);
-    record2.createdAt = new Date();
-    this.recordList.push(record2);
+    recordListModel.create(this.record)
   }
   @Watch('recordList')
   onRecordListChange() {
