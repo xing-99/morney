6fc76a27ed7353550aca9c22e3ebfe9506fff90c
xing <582158539@qq.com>
2021-01-05 21:22:45 +0800

    更新CSS

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -2,26 +2,27 @@
   <ul class="tabs" :class="{[classPrefix+'-tabs']: classPrefix}">
     <li v-for="item in dataSource" :key="item.value"
         class="tabs-item" :class="{selected: item.value === value, [classPrefix+'-tabs-item']: classPrefix}"
-    @click="select(item)">{{item.text}}</li>
+        @click="select(item)">{{ item.text }}
+    </li>
   </ul>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component, Prop} from 'vue-property-decorator';
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
 
-  type dataSourceItem = {text: string; value: string}
-  @Component
-  export default class Tabs extends Vue {
-    @Prop({required: true, type: Array})
-    dataSource!: dataSourceItem[]
-    @Prop(String) readonly value!: string
-    @Prop(String) classPrefix?: string
+type dataSourceItem = { text: string; value: string }
+@Component
+export default class Tabs extends Vue {
+  @Prop({required: true, type: Array})
+  dataSource!: dataSourceItem[];
+  @Prop(String) readonly value!: string;
+  @Prop(String) classPrefix?: string;
 
-    select(item: dataSourceItem) {
-      this.$emit('update:value', item.value)
-    }
+  select(item: dataSourceItem) {
+    this.$emit('update:value', item.value);
   }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -31,23 +32,23 @@
   text-align: center;
   font-size: 24px;
 
-&-item {
-  width: 50%;
-  height: 64px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
+  &-item {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
 
-&.selected::after {
-   content: '';
-   position: absolute;
-   bottom: 0;
-   left: 0;
-   width: 100%;
-   height: 4px;
-   background: #333;
- }
-}
+    &.selected::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
 }
 </style>
